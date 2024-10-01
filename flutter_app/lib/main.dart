0bb4d52acbc826fdb5b0fb2786bc000eb06a7cb8
Miquel Moyano <miquel.moyano@gmail.com>
2024-10-01 14:14:58 +0200

    Creado el proyecto inicial

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(flutterApp());
+}
+
+Widget flutterApp() {
+  // Crea un ValueNotifier para el contador
+  final ValueNotifier<int> contadorNotifier = ValueNotifier<int>(0);
+
+  return MaterialApp(
+    title: 'Flutter App',
+    theme: ThemeData(
+      primarySwatch: Colors.orange,
+    ),
+    home: Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(100),
+        child: Container(
+          padding: const EdgeInsets.only(top: 20),
+          child: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color.fromARGB(255, 128, 19, 3), width: 2),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              padding: const EdgeInsets.all(8),
+              child: const Text(
+                'Flutter App',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 240, 65, 1),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ValueListenableBuilder<int>(
+              valueListenable: contadorNotifier,
+              builder: (context, contador, child) {
+                return Text(
+                  'Contador: $contador',
+                  style: const TextStyle(
+                    fontSize: 40,
+                    color: Color.fromARGB(255, 240, 65, 1),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    ),
+  );
+}
